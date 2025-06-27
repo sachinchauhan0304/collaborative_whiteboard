bff6e5e4e20d988e2ef03fc69751dd7731e0fec2
sachinchauhan0304 <sachin_c@ch.iitr.ac.in>
2025-06-27 06:49:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,18 @@ tool = st.sidebar.radio("Tool", ["freedraw", "line", "rect", "circle", "transfor
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 20, 5)
 stroke_color = st.sidebar.color_picker("Stroke color", "#FF0000")
 bg_color = st.sidebar.color_picker("Background color", "#FFFFFF")
-bg_image = None
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
+# Simulated eraser (white stroke)
+if st.sidebar.button("Use Eraser"):
+    stroke_color = bg_color
 
 # Canvas component
 canvas_result = st_canvas(
-    fill_color="rgba(255, 255, 255, 0.0)",  # Transparent fill
+    fill_color="rgba(255, 255, 255, 0.0)",  # Transparent fill for shapes
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
-    background_image=bg_image,
     update_streamlit=realtime_update,
     height=500,
     width=800,
